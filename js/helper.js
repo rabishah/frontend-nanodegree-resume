a57cbd467ce14a098d4ecbdf2bb828890de229c3
Rabi Shah <green.rabi@hotmail.com>
2016-05-10 23:31:07 +0530

    Init

--- js/helper.js
+++ js/helper.js
@@ -1,17 +1,3 @@
-/*
-
-This file contains all of the code running in the background that makes resumeBuilder.js possible. We call these helper functions because they support your code in this course.
-
-Don't worry, you'll learn what's going on in this file throughout the course. You won't need to make any changes to it until you start experimenting with inserting a Google Map in Problem Set 3.
-
-Cameron Pittman
-*/
-
-
-/*
-These are HTML strings. As part of the course, you'll be using JavaScript functions
-replace the %data% placeholder text you see in them.
-*/
 var HTMLheaderName = '<h1 id="name">%data%</h1>';
 var HTMLheaderRole = '<span>%data%</span><hr>';
 
@@ -58,10 +44,6 @@ var HTMLonlineURL = '<br><a href="#">%data%</a>';
 var internationalizeButton = '<button>Internationalize</button>';
 var googleMap = '<div id="map"></div>';
 
-
-/*
-The International Name challenge in Lesson 2 where you'll create a function that will need this helper code to run. Don't delete! It hooks up your code to the button you'll be appending.
-*/
 $(document).ready(function() {
   $('button').click(function() {
     var $name = $('#name');
@@ -70,9 +52,7 @@ $(document).ready(function() {
   });
 });
 
-/*
-The next few lines about clicks are for the Collecting Click Locations quiz in Lesson 2.
-*/
+
 clickLocations = [];
 
 function logClicks(x,y) {
@@ -86,22 +66,11 @@ function logClicks(x,y) {
 }
 
 $(document).click(function(loc) {
-  // your code goes here!
+  logClicks(loc.pageX, loc.pageY);
 });
 
-
-
-/*
-This is the fun part. Here's where we generate the custom Google Map for the website.
-See the documentation below for more details.
-https://developers.google.com/maps/documentation/javascript/reference
-*/
 var map;    // declares a global map variable
 
-
-/*
-Start here! initializeMap() is called when page is loaded.
-*/
 function initializeMap() {
 
   var locations;
@@ -110,38 +79,22 @@ function initializeMap() {
     disableDefaultUI: true
   };
 
-  /* 
-  For the map to be displayed, the googleMap var must be
-  appended to #mapDiv in resumeBuilder.js. 
-  */
   map = new google.maps.Map(document.querySelector('#map'), mapOptions);
 
 
-  /*
-  locationFinder() returns an array of every location string from the JSONs
-  written for bio, education, and work.
-  */
   function locationFinder() {
 
     // initializes an empty array
     var locations = [];
 
     // adds the single location property from bio to the locations array
-    locations.push(bio.contacts.location);
+    locations.push(info.bio.contacts.location);
 
-    // iterates through school locations and appends each location to
-    // the locations array. Note that forEach is used for array iteration
-    // as described in the Udacity FEND Style Guide: 
-    // https://udacity.github.io/frontend-nanodegree-styleguide/javascript.html#for-in-loop
-    education.schools.forEach(function(school){
+    info.education.schools.forEach(function(school){
       locations.push(school.location);
     });
 
-    // iterates through work locations and appends each location to
-    // the locations array. Note that forEach is used for array iteration
-    // as described in the Udacity FEND Style Guide: 
-    // https://udacity.github.io/frontend-nanodegree-styleguide/javascript.html#for-in-loop
-    work.jobs.forEach(function(job){
+    info.work.jobs.forEach(function(job){
       locations.push(job.location);
     });
 
@@ -234,16 +187,8 @@ function initializeMap() {
 
 }
 
-/*
-Uncomment the code below when you're ready to implement a Google Map!
-*/
+window.addEventListener('load', initializeMap);
 
-// Calls the initializeMap() function when the page loads
-//window.addEventListener('load', initializeMap);
-
-// Vanilla JS way to listen for resizing of the window
-// and adjust map bounds
-//window.addEventListener('resize', function(e) {
-  //Make sure the map bounds get updated on page resize
-//  map.fitBounds(mapBounds);
-//});
+window.addEventListener('resize', function(e) {
+ map.fitBounds(mapBounds);
+});
--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,186 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var info = {
+  "bio": {
+    "name": "Rabi C Shah",
+    "role": "Frontend Engineer",
+    "contacts": {
+      "mobile": "+91-9008498419",
+      "email": "green.rabi@hotmail.com",
+      "github": "rabishah",
+      "twitter": "rabi_shah89",
+      "location": "Bangalore, Karnataka",
+      "blog": "blog.27ae60.com"
+    },
+    "welcomeMessage": "Soft Kitty, Warm Kittly, Little ball of fur...",
+    "skills": ["Javascript", "Awesome"],
+    "biopic": "http://i.imgur.com/53Ack3j.jpg",
+    "display": function($header, $topContacts) {
+      var $skillsStart = $(HTMLskillsStart);
+      var skills = this.skills;
+      var contacts = this.contacts;
+
+      $header.prepend($(HTMLheaderName.replace("%data%", this.name)));
+      $header.prepend($(HTMLheaderRole.replace("%data%", this.role)));
+
+      /* contacts */
+      $topContacts.append($(HTMLmobile.replace("%data%", contacts.mobile)));
+      $topContacts.append($(HTMLemail.replace("%data%", contacts.email)));
+      $topContacts.append($(HTMLtwitter.replace("%data%", contacts.twitter)));
+      $topContacts.append($(HTMLgithub.replace("%data%", contacts.github)));
+      $topContacts.append($(HTMLlocation.replace("%data%", contacts.location)));
+
+      $header.append($(HTMLbioPic.replace("%data%", this.biopic)));
+      $header.append($(HTMLwelcomeMsg.replace("%data%", this.welcomeMessage)));
+
+      $.each(skills, function(k, v) {
+        $($skillsStart[1]).append($(HTMLskills.replace("%data%", v)));
+      });
+      $header.append($skillsStart);
+
+      $("#footerContacts").append($(HTMLmobile.replace("%data%", contacts.mobile)));
+    }
+  },
+  "work": {
+    "jobs": [{
+      "employer": "Minsh",
+      "title": "Web Hacker",
+      "location": "Bangalore, Karnataka",
+      "dates": "inprogress",
+      "description": "Blah! Blah! Blah!"
+    }],
+    "display": function($workExperience) {
+      var $workStart = $(HTMLworkStart);
+      var jobs = this.jobs;
+
+      $.each(jobs, function(k, v) {
+        var title = HTMLworkEmployer.replace("%data%", v.employer);
+        var employer = HTMLworkTitle.replace("%data%", v.title);
+
+        $workStart.append($(title + employer));
+        $workStart.append($(HTMLworkDates.replace("%data%", v.dates)));
+        $workStart.append($(HTMLworkLocation.replace("%data%", v.location)));
+        $workStart.append($(HTMLworkDescription.replace("%data%", v.description)));
+      });
+
+      $workExperience.append($workStart);
+    }
+  },
+  "education": {
+    "schools" : [
+      {
+        "name" : "Indian Institute of Information Technology - Allahabad",
+        "location" : "Allahabad, Uttar Pradesh",
+        "degree" : "Bachelor", 
+        "majors" : "Information Technology",
+        "dates" : "2009 - 2013",
+        "url" : "https://www.iiita.ac.in"
+      },
+      {
+        "name" : "St. Xavier's College",
+        "location" : "Kathmandu, Nepal",
+        "degree" : "Intermediate of Science", 
+        "majors" : "Physics, Chemistry and Mathematics",
+        "dates" : "2007 - 2008",
+        "url" : "http://sxc.edu.np"
+      }
+    ],
+    "display": function($education) {
+      var schools = this.schools;
+      this.schools.forEach(function(school) {
+        $education.append(HTMLschoolStart);
+        var formattedName = HTMLschoolName.replace("%data%", school.name);
+        var formattedDegree = HTMLschoolDegree.replace("%data%", school.degree);
+        var formattedDates = HTMLschoolDates.replace("%data%", school.dates);
+        var formattedLocation = HTMLschoolLocation.replace("%data%", school.location);
+        var formattedMajor = HTMLschoolMajor.replace("%data%", school.majors);
+        $(".education-entry:last").append(formattedName + formattedDegree,formattedDates,formattedLocation,formattedMajor);
+      });
+    }
+  },
+  "projects" : {
+    "projects" : [{
+      "title" : "Nepali Unicode",
+      "dates" : "2012",
+      "description" : "Phoenotic translation of Ennglish words to Nepali words. https://rabishah.github.io/nepali-unicode/",
+      "images" : ["images/1.jpg","images/2.jpg","images/3.jpg","images/4.jpg"]
+    }],
+    "display": function($projects) {
+      this.projects.forEach(function(project) {
+        $projects.append(HTMLprojectStart);
+        var formattedTitle = HTMLprojectTitle.replace("%data%",project.title);
+        var formattedDates = HTMLprojectDates.replace("%data%",project.dates);
+        var formattedDescription = HTMLprojectDescription.replace("%data%",project.description);
+
+        $(".project-entry:last").append(formattedTitle,formattedDates,formattedDescription);
+        for (image in project.images) {
+          var formattedImage = HTMLprojectImage.replace("%data%",project.images[image]);
+          $(".project-entry:last").append(formattedImage);
+        };
+      });
+    }
+  }
+}
+
+$(function() {
+  var model = {
+    "init": function() {
+      this.info = info;
+    },
+    "getInfo": function() {
+      return this.info;
+    }
+  };
+
+  var octopus = {
+    "init": function() {
+      model.init();
+      view.init();
+    },
+    "getInfo": function(category, elm) {
+      var info = model.getInfo()[category];
+      if (elm) {
+        return info[elm];
+      }
+
+      return info;
+    }
+  };
+
+  var view = {
+    "init": function() {
+      this.$header = $('#header');
+      this.$topContacts = $('#topContacts');
+      this.$workExperience = $('#workExperience');
+      this.$projects = $('#projects');
+      this.$education = $('#education');
+
+      this.render.bio.call(this);
+      this.render.work.call(this);
+      this.render.education.call(this);
+      this.render.projects.call(this);
+
+      $("#main").append(internationalizeButton);
+      $("#mapDiv").append(googleMap);
+    },
+
+    "render": {
+      'bio': function() {
+        var bioInfo = octopus.getInfo('bio');
+        bioInfo.display.call(bioInfo, this.$header, this.$topContacts);
+      },
+      'work': function() {
+        var workInfo = octopus.getInfo('work');
+        workInfo.display.call(workInfo, this.$workExperience);
+      },
+      'education': function() {
+        var educationInfo = octopus.getInfo('education');
+        educationInfo.display.call(educationInfo, this.$education);
+      },
+      'projects': function() {
+        var projectsInfo = octopus.getInfo('projects');
+        projectsInfo.display.call(projectsInfo, this.$projects);
+      }
+    }
+  };
+
+  octopus.init();
+}());
